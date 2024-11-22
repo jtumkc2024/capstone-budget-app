11ef725985f84bc8a966ae7b30ec5ed768a002ea
jtumkc2024 <Capstone451JM@protonmail.com>
2024-11-21 18:57:13 -0600

    Delete src directory

--- src/models/Project.ts
+++
@@ -1,9 +0,0 @@
-export interface Project {
-    projectId: number;
-    userId: number;
-    projectTitle: string;
-    projectDescription: string;
-    renewable: number;
-    priority: number;
-    deadline: string;
-}
