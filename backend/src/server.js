e8894eb5b434b30be476a237bcdff2168bac550e
jtumkc2024 <Capstone451JM@protonmail.com>
2024-11-21 18:56:25 -0600

    converted backend to js

---
+++ backend/src/server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const mysql = require('mysql2/promise');
+const cors = require('cors');
+
+const app = express();
+const port = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+const databaseConfig = {
+  host: '192.168.215.2',
+  port: 3306,
+  user: 'root',
+  password: '1234qwerASDF',
+  database: 'sys'
+};
+
+app.get('/api/users', async (_, res) => {
+  try {
+    const connection = await mysql.createConnection(databaseConfig);
+    const [rows] = await connection.query('SELECT * FROM User');
+    res.json(rows);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get('/api/users/count', async (req, res) => {
+    try {
+      const connection = await mysql.createConnection(databaseConfig);
+      const [rows] = await connection.query('SELECT COUNT(*) AS total_users FROM User');
+      res.json(rows[0]);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+
+app.get('/api/users/:id', async (req, res) => {
+    try {
+        const connection = await mysql.createConnection(databaseConfig);
+        const [rows] = await connection.query('select * from Project p where p.user_id = ?', [req.params.id]);
+        res.json(rows);        
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
+
+// app.get(## ENDPOINT PATH ##, async (req, res) => {
+//   try {
+//       const connection = await mysql.createConnection(databaseConfig);
+//       const [rows] = await connection.query(## QUERY ##, ## QUERY PARAMS ##);
+//       res.json(rows);
+//   } catch (error: any) {
+//       res.status(500).json({ error: error.message });
+//   }
+// })
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
